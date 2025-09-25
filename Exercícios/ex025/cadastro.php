c53066541826623e020ce32521f9241e286aaa4d
CCDiogo <232976525+CCDiogo@users.noreply.github.com>
2025-09-24 22:16:23 -0300

    Testes de fomulários + php fake

---
+++ Exercícios/ex025/cadastro.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Você ainda n sabe mexer com isso!</h1>
+</body>
+</html>
